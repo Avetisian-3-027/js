bc3945c6391abb7152891025e592f8addc13d81a
Avetisian-3-027 <novel.rush@krfk.kai.edu.ua>
2025-04-06 22:06:50 +0200

    lab2

---
+++ LAB2/lab2.js
@@ -0,0 +1,68 @@
+//------
+//ex1-2|
+//------
+
+/*
+var a = "TEXT"
+let b = true
+const c = 42
+
+console.log(typeof(a), typeof(b), typeof(c))
+*/
+
+//----
+//ex3|
+//----
+
+/*
+let user = {
+    student_name: "",
+    student_age: 0,
+    student_status: true
+}
+*/
+
+//----
+//ex4|
+//----
+
+/*
+let struct = {
+    student_name: "Test",
+    student_age: 0,
+    student_status: true,
+    university: "tEsT"
+}
+
+console.log(struct)
+*/
+
+//----
+//ex5|
+//----
+
+/*
+let b = true
+const c = 42
+
+let struct = {
+    student_name: "Test",
+    student_age: 0,
+    student_status: true,
+    university: "tEsT"
+}
+
+if(true) {
+    let b = "Tatar"
+    console.log(b)
+}
+console.log(b)
+*/
+//----
+//ex6|
+//----
+/*
+const c = 42
+c = 23
+console.log(c)
+*/
