834a68fd9500218f34789156ea0b49e5c4a659d4
Avetisian-3-027 <novel.rush@krfk.kai.edu.ua>
2025-04-06 22:18:53 +0200

    add lab1 and lab3

---
+++ LAB3/lab3.js
@@ -0,0 +1,41 @@
+//----
+//ex1-3|
+//----
+/*
+let a = "string"
+let b = 0
+let c = false
+let d
+let e = null
+
+a = 12
+b = "22"
+e = true
+
+console.log(b + a)
+*/
+
+//----
+//ex4|
+//----
+/*
+function whatType(value) {
+    return typeof value
+}
+console.log(whatType(42))
+console.log(whatType("Hello"))
+console.log(whatType(true))
+console.log(whatType({}))
+console.log(whatType([]))
+console.log(whatType(null))
+console.log(whatType(undefined))
+console.log(whatType(() => {}))
+*/
+
+//----
+//ex5|
+//----
+/*
+console.log(1 === "1")
+console.log(1 == "1")
+*/
