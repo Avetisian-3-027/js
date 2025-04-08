8fc50546bdf937a06e8863b1f0788e528f04236e
Avetisian-3-027 <novel.rush@krfk.kai.edu.ua>
2025-04-08 14:25:27 +0200

    Remade, for clarity

--- LAB4/lab4_add.js
+++ LAB4/lab4_add.js
@@ -1,20 +1,21 @@
 function gradeLevelDisplay(argue) {
     if (argue < 0 || argue > 100 || isNaN(argue)) {// защита от дурака
         alert("Некорректный ввод")
-        return 0
+        return 1
     }
     else if (argue <= 49)
-        return alert("Тест не сдан! Балл слишком низок...") 
+        alert("Тест не сдан! Балл слишком низок...") 
     else if (argue <= 69)
-        return alert("Тест сдан! Балл удовлитворительный...")
+        alert("Тест сдан! Балл удовлитворительный...")
     else if (argue <= 89)
-        return alert("Тест сдан! Балл хороший...")
+        alert("Тест сдан! Балл хороший...")
     else if (argue <= 100)
-        return alert("Тест сдан! Балл отличный...")
+        alert("Тест сдан! Балл отличный...")
+    return 0
 }
 
 while(true) { // мне надоело по сто раз перезапускать html файл
-    if (gradeLevelDisplay(parseInt(prompt("Введите свой балл: "), 10)) != false)
+    if (!(gradeLevelDisplay(parseInt(prompt("Введите свой балл: "), 10))))
         break
 }
 close()
